2712157f8c2f377af7e50594d34f7226498e333c
Ikram Abdiaziz <ikramdaud999@gmail.com>
2025-08-12 11:14:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';  // const means the value won't change
+
+let mostProfitableNeighborhood = 'Chelsea';  // let means the value can change later
+
+let companyCeo = 'Susan Smith';  // let also allows reassignment
